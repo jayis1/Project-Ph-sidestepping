72e7688fb690fd20b0990e64c42dc08e15892bf8
jayis1 <jayis1@github.com>
2026-04-13 21:08:12 +0200

    Patch actual dynamic AutoModel instances instead of underlying template class

--- vibevoice-api/main.py
+++ vibevoice-api/main.py
@@ -11,60 +11,30 @@ import transformers.models.auto.configuration_auto
 import transformers.models.auto.processing_auto
 
 # --- PREVENT CONFLICTS BETWEEN HUGGINGFACE AND MICROSOFT REPO ---
-_orig_model_reg = transformers.models.auto.auto_factory._BaseAutoModelClass.register
-@classmethod
-def _safe_model_reg(cls, *args, **kwargs):
-    try:
-        if 'exist_ok' not in kwargs:
-            kwargs['exist_ok'] = True
-        return _orig_model_reg.__func__(cls, *args, **kwargs)
-    except TypeError:
-        kwargs.pop('exist_ok', None)
-        try: return _orig_model_reg.__func__(cls, *args, **kwargs)
-        except ValueError as e:
-            if "already used" in str(e): pass
-            else: raise
-    except ValueError as e:
-        if "already used" in str(e): pass
-        else: raise
-transformers.models.auto.auto_factory._BaseAutoModelClass.register = _safe_model_reg
+from transformers import AutoModel, AutoModelForCausalLM, AutoConfig, AutoProcessor
 
-_orig_conf_reg = transformers.models.auto.configuration_auto.AutoConfig.register
-@classmethod
-def _safe_conf_reg(cls, *args, **kwargs):
-    try:
-        if 'exist_ok' not in kwargs:
-            kwargs['exist_ok'] = True
-        return _orig_conf_reg.__func__(cls, *args, **kwargs)
-    except TypeError:
-        kwargs.pop('exist_ok', None)
-        try: return _orig_conf_reg.__func__(cls, *args, **kwargs)
-        except ValueError as e:
-            if "already used" in str(e): pass
-            else: raise
-    except ValueError as e:
-        if "already used" in str(e): pass
-        else: raise
-transformers.models.auto.configuration_auto.AutoConfig.register = _safe_conf_reg
-
-if hasattr(transformers.models.auto.processing_auto.AutoProcessor, 'register'):
-    _orig_proc_reg = transformers.models.auto.processing_auto.AutoProcessor.register
+def _patch_exist_ok(cls):
+    if not hasattr(cls, 'register'): return
+    orig_reg = cls.register
     @classmethod
-    def _safe_proc_reg(cls, *args, **kwargs):
+    def safe_reg(c, *args, **kwargs):
         try:
             if 'exist_ok' not in kwargs:
                 kwargs['exist_ok'] = True
-            return _orig_proc_reg.__func__(cls, *args, **kwargs)
+            return orig_reg.__func__(c, *args, **kwargs)
         except TypeError:
             kwargs.pop('exist_ok', None)
-            try: return _orig_proc_reg.__func__(cls, *args, **kwargs)
+            try: return orig_reg.__func__(c, *args, **kwargs)
             except ValueError as e:
                 if "already used" in str(e): pass
                 else: raise
         except ValueError as e:
             if "already used" in str(e): pass
             else: raise
-    transformers.models.auto.processing_auto.AutoProcessor.register = _safe_proc_reg
+    cls.register = safe_reg
+
+for hf_class in [AutoModel, AutoModelForCausalLM, AutoConfig, AutoProcessor]:
+    _patch_exist_ok(hf_class)
 # ----------------------------------------------------------------
 
 app = FastAPI()
